be11569ddd66a7dd52c66042eac07b8849d5d79f
KenTaR0-gg <kotya.piso@gmail.com>
2025-06-25 05:48:31 +0300

    module 3. Final

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,7 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
-        byte p = (byte) number;
-        System.out.println(p);
+
+        int a = number / 100;
+        int b = (number - a * 100) / 10;
+        int c = number - a * 100 - b * 10;
+
+        number = a*100 + b*10 + c;
+        System.out.println(100*c + 10*b +a);
     }
 }
